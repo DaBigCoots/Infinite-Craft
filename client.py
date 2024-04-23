0d91bdc5f740e44062e2e57da4486fcf799b4202
DaBigCoots <skersten87@gmail.com>
2024-04-23 12:33:48 -0600

    Update client with new headers and session for keepig cookies per new server requirements.

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 import requests
 
+session = None
+
 
 def send_request(item1, item2):
     payload = {"first": item1, "second": item2}
     url = "https://neal.fun/api/infinite-craft/pair"
-    headers = {"Referer": "https://neal.fun/infinite-craft/"}
-    x = requests.get(url, headers=headers, params=payload)
+    global session
+    if session is None:
+        session = create_session()
+    session.headers.update({"Referer": "https://neal.fun/infinite-craft/"})
+    x = session.get(url, params=payload)
     return x
 
 
@@ -17,3 +22,17 @@ def parse_json(json_body, key):
         print("Missing '" + key + "' key in JSON data")
     finally:
         return value
+
+
+def create_session():
+    print("Creating Session")
+    url = "https://neal.fun/api/infinite-craft"
+    session = requests.Session()
+    session.headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+            "Host": "neal.fun",
+        }
+    )
+    r = session.get(url)
+    return session
